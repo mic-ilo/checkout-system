41060d7416d75f4fcef1da92e5e7c341f723bbec
mic-ilo <michelaquino1003@gmail.com>
2024-01-21 04:31:52 +0800

    added cartItemCart component and total cart amount

---
+++ frontend/src/components/CartItemCard.tsx
@@ -0,0 +1,49 @@
+import { useEffect } from "react";
+
+interface Props {
+  name: string;
+  price: number;
+  qty: number;
+  image: string;
+  setTotal: React.Dispatch<React.SetStateAction<number[]>>;
+}
+
+export default function CartItemCard({
+  name,
+  price,
+  qty,
+  image,
+  setTotal,
+}: Props) {
+  const totalPerItem: (price: number, qty: number) => number = (price, qty) => {
+    const total = price * qty;
+    return Number(total.toFixed(2));
+  };
+
+  useEffect(() => {
+    setTotal((prevTotal) => [...prevTotal, totalPerItem(price, qty)]);
+  }, [price, qty, setTotal]);
+
+  return (
+    <div className="max-w-7xl mx-auto">
+      <div className="flex items-center gap-2">
+        <div className="object-cover w-60 ">
+          <img src={`../src/assets/${image}`} alt={name} />
+        </div>
+        <div>
+          <p className="font-bold">{name}</p>
+          <p>
+            <span className="font-bold">Price:</span> $ {price}
+          </p>
+          <p>
+            <span className="font-bold">Quantity:</span> {qty}
+          </p>
+          <p>
+            <span className="font-bold">Total:</span> ${" "}
+            {totalPerItem(price, qty)}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -1,25 +1,52 @@
 import { useContext } from "react";
 import { CartContext } from "../App";
 import dataDB from "../data/items.json";
+import CartItemCard from "../components/CartItemCard";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function Cart() {
   const [cart] = useContext(CartContext);
+  const [total, setTotal] = useState<number[]>([]);
+  const totalAmount = total.reduce((acc, curr) => acc + curr, 0).toFixed(2);
+  console.log(total);
 
-  const itemsInCart = dataDB.filter((item) => cart.some((cartItem) => cartItem.id === item.uuid))
+  const itemsInCart = dataDB.filter((item) =>
+    cart.some((cartItem) => cartItem.id === item.uuid)
+  );
 
   return (
-    <div>
-    {itemsInCart.map((item) => {
-      const cartItem = cart.find((cartItem) => cartItem.id === item.uuid);
+    <div className="max-w-7xl mx-auto p-3 md:p-10">
+      <h1 className="text-3xl font-bold underline text-center mt-10">
+        ADDED TO CART
+      </h1>
+      {cart.length ? (
+        <div>
+          {itemsInCart.map((item) => {
+            const cartItem = cart.find((cartItem) => cartItem.id === item.uuid);
 
-      return (
-        <div key={item.uuid}>
-          <p>{item.name}</p>
-          <p>${item.price}</p>
-          <p>Quantity: {cartItem && cartItem.qty}</p>
+            return (
+              <div key={item.uuid}>
+                <CartItemCard
+                  name={item.name}
+                  price={Number(item.price)}
+                  qty={cartItem ? cartItem.qty : 0}
+                  image={item.image}
+                  setTotal={setTotal}
+                />
+              </div>
+            );
+          })}
+          <p>Total Amount: {totalAmount}</p>
         </div>
-      );
-    })}
-  </div>
-  )
+      ) : (
+        <p>
+          No items in the cart.{" "}
+          <Link to="/" className="underline text-yellow-500 font-bold">
+            Browse items
+          </Link>
+        </p>
+      )}
+    </div>
+  );
 }
