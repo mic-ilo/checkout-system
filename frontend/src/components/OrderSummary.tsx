89945ec07f89339877fc4adbdcb6c5ea23d98cf9
mic-ilo <michelaquino1003@gmail.com>
2024-01-23 02:16:30 +0800

    separated order summmary into a component

---
+++ frontend/src/components/OrderSummary.tsx
@@ -0,0 +1,61 @@
+import { Link } from "react-router-dom";
+
+interface Props {
+  totalAmount: number;
+}
+
+export default function OrderSummary({ totalAmount }: Props) {
+  const discount = (amount: number) => {
+    if (amount > 100) {
+      return "20% voucher applied";
+    } else if (amount > 50) {
+      return "15% voucher applied";
+    } else if (amount > 20) {
+      return "10% discount applied";
+    } else {
+      return "no voucher applied";
+    }
+  };
+
+  const grandTotal = (amount: number) => {
+    if (amount > 100) {
+      return (amount * 0.8).toFixed(2);
+    } else if (amount > 50) {
+      return (amount * 0.85).toFixed(2);
+    } else if (amount > 20) {
+      return (amount * 0.9).toFixed(2);
+    } else {
+      return amount;
+    }
+  };
+
+  return (
+    <div className=" bg-slate-200 h-fit p-5 bg-opacity-45 rounded-lg text-gray-700">
+      <h2 className=" text-2xl text-center font-light pb-5">Order summary</h2>
+      <p className="">
+        <span className="font-bold">Sub total: </span>$ {totalAmount}
+      </p>
+      <p>
+        <span className="font-bold">Discount: </span>
+        {discount(Number(totalAmount))}{" "}
+      </p>
+      <p>
+        <span className="font-bold">Total: </span> ${" "}
+        {grandTotal(Number(totalAmount))}
+      </p>
+      <button
+        type="button"
+        className=" mt-5 bg-amber-500 font-bold text-black p-2 rounded-lg w-full"
+      >
+        PROCEED TO CHECKOUT
+      </button>
+      <Link
+        to="/"
+        type="button"
+        className=" mt-5 bg-slate-300 font-bold text-black text-center p-2 rounded-lg w-full"
+      >
+        CONTINUE SHOPPING {">>"}
+      </Link>
+    </div>
+  );
+}
--- frontend/src/pages/Cart.tsx
+++ frontend/src/pages/Cart.tsx
@@ -6,11 +6,15 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { useEffect } from "react";
 
+import OrderSummary from "../components/OrderSummary";
+
 export default function Cart() {
   const [cart, setCart] = useContext(CartContext)!.cartState;
   const [total, setTotal] = useState<{ id: number; total: number }[]>([]);
 
-  const totalAmount = total.reduce((acc, curr) => acc + curr.total, 0).toFixed(2);
+  const totalAmount = total
+    .reduce((acc, curr) => acc + curr.total, 0)
+    .toFixed(2);
 
   useEffect(() => {
     const storedCart = localStorage.getItem("cartItems");
@@ -24,30 +28,6 @@ export default function Cart() {
   const itemsInCart = dataDB.filter((item) =>
     cart.some((cartItem) => cartItem.id === item.uuid)
   );
-
-  const discount = (amount: number) => {
-    if (amount > 100) {
-      return "20% voucher applied";
-    } else if (amount > 50) {
-      return "15% voucher applied";
-    } else if (amount > 20) {
-      return "10% discount applied";
-    } else {
-      return "no voucher applied";
-    }
-  };
-
-  const grandTotal = (amount: number) => {
-    if (amount > 100) {
-      return (amount * 0.8).toFixed(2);
-    } else if (amount > 50) {
-      return (amount * 0.85).toFixed(2);
-    } else if (amount > 20) {
-      return (amount * 0.9).toFixed(2);
-    } else {
-      return amount;
-    }
-  };
 
   return (
     <div className="max-w-7xl mx-auto p-3">
@@ -76,34 +56,8 @@ export default function Cart() {
               );
             })}
           </div>
-          <div className="md:w-1/3 bg-slate-200 h-fit p-5 bg-opacity-45 rounded-lg text-gray-700">
-            <h2 className=" text-2xl text-center font-light pb-5">
-              Order summary
-            </h2>
-            <p className="">
-              <span className="font-bold">Sub total: </span>$ {totalAmount}
-            </p>
-            <p>
-              <span className="font-bold">Discount: </span>
-              {discount(Number(totalAmount))}{" "}
-            </p>
-            <p>
-              <span className="font-bold">Total: </span> ${" "}
-              {grandTotal(Number(totalAmount))}
-            </p>
-            <button
-              type="button"
-              className=" mt-5 bg-amber-500 font-bold text-black p-2 rounded-lg w-full"
-            >
-              PROCEED TO CHECKOUT
-            </button>
-            <Link
-              to="/"
-              type="button"
-              className=" mt-5 bg-slate-300 font-bold text-black text-center p-2 rounded-lg w-full"
-            >
-              CONTINUE SHOPPING {">>"}
-            </Link>
+          <div className="md:w-1/3">
+            <OrderSummary totalAmount={Number(totalAmount)} />
           </div>
         </div>
       ) : (
